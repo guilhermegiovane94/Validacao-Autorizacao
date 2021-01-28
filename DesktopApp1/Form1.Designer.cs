36b2233832c3f93b5fdc6fbf011bd85bc070a000
guilhermegiovane94 <31057215+guilhermegiovane94@users.noreply.github.com>
2021-01-28 16:29:08 -0300

    last fix

--- DesktopApp1/Form1.Designer.cs
+++ DesktopApp1/Form1.Designer.cs
@@ -45,114 +45,123 @@ namespace DesktopApp1
             this.cmbVERSION = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.txtDATA_LOG = new System.Windows.Forms.MaskedTextBox();
+            this.check400 = new System.Windows.Forms.CheckBox();
+            this.cmbBreakLine = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // helloWorldLabel
             // 
             this.helloWorldLabel.AutoSize = true;
             this.helloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.helloWorldLabel.Location = new System.Drawing.Point(232, 25);
+            this.helloWorldLabel.Location = new System.Drawing.Point(174, 20);
+            this.helloWorldLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.helloWorldLabel.Name = "helloWorldLabel";
-            this.helloWorldLabel.Size = new System.Drawing.Size(256, 31);
+            this.helloWorldLabel.Size = new System.Drawing.Size(209, 26);
             this.helloWorldLabel.TabIndex = 3;
             this.helloWorldLabel.Text = "EASY VALIDATION";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 80);
+            this.label1.Location = new System.Drawing.Point(42, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.Size = new System.Drawing.Size(30, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "NSU";
             // 
             // txtNSU
             // 
-            this.txtNSU.Location = new System.Drawing.Point(59, 101);
-            this.txtNSU.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNSU.Location = new System.Drawing.Point(44, 82);
+            this.txtNSU.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNSU.Name = "txtNSU";
-            this.txtNSU.Size = new System.Drawing.Size(371, 22);
+            this.txtNSU.Size = new System.Drawing.Size(279, 20);
             this.txtNSU.TabIndex = 6;
             this.txtNSU.Text = "441735";
             // 
             // txtFULL_NSU
             // 
-            this.txtFULL_NSU.Location = new System.Drawing.Point(59, 155);
-            this.txtFULL_NSU.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFULL_NSU.Location = new System.Drawing.Point(44, 126);
+            this.txtFULL_NSU.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFULL_NSU.Name = "txtFULL_NSU";
-            this.txtFULL_NSU.Size = new System.Drawing.Size(371, 22);
+            this.txtFULL_NSU.Size = new System.Drawing.Size(279, 20);
             this.txtFULL_NSU.TabIndex = 8;
             this.txtFULL_NSU.Text = "NSU:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 134);
+            this.label2.Location = new System.Drawing.Point(42, 109);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "FULL NSU";
             // 
             // txtLOG_PATH
             // 
             this.txtLOG_PATH.Enabled = false;
-            this.txtLOG_PATH.Location = new System.Drawing.Point(59, 214);
-            this.txtLOG_PATH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtLOG_PATH.Location = new System.Drawing.Point(44, 174);
+            this.txtLOG_PATH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLOG_PATH.Name = "txtLOG_PATH";
-            this.txtLOG_PATH.Size = new System.Drawing.Size(371, 22);
+            this.txtLOG_PATH.Size = new System.Drawing.Size(279, 20);
             this.txtLOG_PATH.TabIndex = 10;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(56, 193);
+            this.label3.Location = new System.Drawing.Point(42, 157);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "LOG PATH";
             // 
             // txtEVIDENCIA_PATH
             // 
             this.txtEVIDENCIA_PATH.Enabled = false;
-            this.txtEVIDENCIA_PATH.Location = new System.Drawing.Point(59, 272);
-            this.txtEVIDENCIA_PATH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEVIDENCIA_PATH.Location = new System.Drawing.Point(44, 221);
+            this.txtEVIDENCIA_PATH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtEVIDENCIA_PATH.Name = "txtEVIDENCIA_PATH";
-            this.txtEVIDENCIA_PATH.Size = new System.Drawing.Size(371, 22);
+            this.txtEVIDENCIA_PATH.Size = new System.Drawing.Size(279, 20);
             this.txtEVIDENCIA_PATH.TabIndex = 12;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(56, 251);
+            this.label4.Location = new System.Drawing.Point(42, 204);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(120, 17);
+            this.label4.Size = new System.Drawing.Size(96, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "EVIDENCIA PATH";
             // 
             // txtBREAK_LINE
             // 
-            this.txtBREAK_LINE.Location = new System.Drawing.Point(59, 326);
-            this.txtBREAK_LINE.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBREAK_LINE.Enabled = false;
+            this.txtBREAK_LINE.Location = new System.Drawing.Point(46, 301);
+            this.txtBREAK_LINE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtBREAK_LINE.Name = "txtBREAK_LINE";
-            this.txtBREAK_LINE.Size = new System.Drawing.Size(371, 22);
+            this.txtBREAK_LINE.Size = new System.Drawing.Size(279, 20);
             this.txtBREAK_LINE.TabIndex = 14;
-            this.txtBREAK_LINE.Text = "*** ACTR ***";
+            this.txtBREAK_LINE.Text = "MENSAGEM=>";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(56, 305);
+            this.label5.Location = new System.Drawing.Point(42, 248);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.Size = new System.Drawing.Size(70, 13);
             this.label5.TabIndex = 13;
             this.label5.Text = "BREAK LINE";
             // 
             // btnVALIDAR
             // 
-            this.btnVALIDAR.Location = new System.Drawing.Point(504, 271);
-            this.btnVALIDAR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnVALIDAR.Location = new System.Drawing.Point(378, 120);
+            this.btnVALIDAR.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnVALIDAR.Name = "btnVALIDAR";
-            this.btnVALIDAR.Size = new System.Drawing.Size(155, 23);
+            this.btnVALIDAR.Size = new System.Drawing.Size(116, 19);
             this.btnVALIDAR.TabIndex = 15;
             this.btnVALIDAR.Text = "VALIDAR";
             this.btnVALIDAR.UseVisualStyleBackColor = true;
@@ -160,10 +169,10 @@ namespace DesktopApp1
             // 
             // btnEVIDENCIA
             // 
-            this.btnEVIDENCIA.Location = new System.Drawing.Point(504, 316);
-            this.btnEVIDENCIA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEVIDENCIA.Location = new System.Drawing.Point(378, 157);
+            this.btnEVIDENCIA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEVIDENCIA.Name = "btnEVIDENCIA";
-            this.btnEVIDENCIA.Size = new System.Drawing.Size(155, 23);
+            this.btnEVIDENCIA.Size = new System.Drawing.Size(116, 19);
             this.btnEVIDENCIA.TabIndex = 16;
             this.btnEVIDENCIA.Text = "GERAR EVIDENCIA";
             this.btnEVIDENCIA.UseVisualStyleBackColor = true;
@@ -172,9 +181,10 @@ namespace DesktopApp1
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(56, 359);
+            this.label6.Location = new System.Drawing.Point(43, 335);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(79, 17);
+            this.label6.Size = new System.Drawing.Size(61, 13);
             this.label6.TabIndex = 17;
             this.label6.Text = "DATA LOG";
             // 
@@ -184,34 +194,56 @@ namespace DesktopApp1
             this.cmbVERSION.Items.AddRange(new object[] {
             "19.2",
             "20.1"});
-            this.cmbVERSION.Location = new System.Drawing.Point(59, 448);
-            this.cmbVERSION.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbVERSION.Location = new System.Drawing.Point(45, 407);
             this.cmbVERSION.Name = "cmbVERSION";
-            this.cmbVERSION.Size = new System.Drawing.Size(369, 24);
+            this.cmbVERSION.Size = new System.Drawing.Size(278, 21);
             this.cmbVERSION.TabIndex = 19;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(56, 428);
+            this.label7.Location = new System.Drawing.Point(43, 391);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(69, 17);
+            this.label7.Size = new System.Drawing.Size(55, 13);
             this.label7.TabIndex = 20;
             this.label7.Text = "VERSION";
             // 
             // txtDATA_LOG
             // 
-            this.txtDATA_LOG.Location = new System.Drawing.Point(59, 388);
+            this.txtDATA_LOG.Location = new System.Drawing.Point(45, 358);
+            this.txtDATA_LOG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDATA_LOG.Mask = "00-00-00";
             this.txtDATA_LOG.Name = "txtDATA_LOG";
-            this.txtDATA_LOG.Size = new System.Drawing.Size(369, 22);
+            this.txtDATA_LOG.Size = new System.Drawing.Size(278, 20);
             this.txtDATA_LOG.TabIndex = 21;
+            // 
+            // check400
+            // 
+            this.check400.AutoSize = true;
+            this.check400.Location = new System.Drawing.Point(378, 84);
+            this.check400.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.check400.Name = "check400";
+            this.check400.Size = new System.Drawing.Size(82, 17);
+            this.check400.TabIndex = 22;
+            this.check400.Text = "é uma 400?";
+            this.check400.UseVisualStyleBackColor = true;
+            // 
+            // cmbBreakLine
+            // 
+            this.cmbBreakLine.FormattingEnabled = true;
+            this.cmbBreakLine.Location = new System.Drawing.Point(46, 264);
+            this.cmbBreakLine.Name = "cmbBreakLine";
+            this.cmbBreakLine.Size = new System.Drawing.Size(279, 21);
+            this.cmbBreakLine.TabIndex = 23;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(851, 596);
+            this.ClientSize = new System.Drawing.Size(638, 484);
+            this.Controls.Add(this.cmbBreakLine);
+            this.Controls.Add(this.check400);
             this.Controls.Add(this.txtDATA_LOG);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.cmbVERSION);
@@ -229,7 +261,7 @@ namespace DesktopApp1
             this.Controls.Add(this.txtNSU);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.helloWorldLabel);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -257,6 +289,8 @@ namespace DesktopApp1
         private System.Windows.Forms.ComboBox cmbVERSION;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.MaskedTextBox txtDATA_LOG;
+        private System.Windows.Forms.CheckBox check400;
+        private System.Windows.Forms.ComboBox cmbBreakLine;
     }
 }
 
